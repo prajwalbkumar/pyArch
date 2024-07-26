d32ac9bd8f41f36ab44c443508b22a1dec8cfdc2
prajwalbkumar <ar.prajwalbkumar@gmail.com>
2024-07-26 17:59:29 +0530

    INIT

---
+++ pyArch.extension/pyArch.tab/Door Data Validator.panel/Acoustic Rating Checks.pushbutton/AcousticRatingCheck_script.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''FLS Doors Validator'''
+__title__ = "Acoustic Rating Checks"
+__author__ = "prajwalbkumar"
+
+
+# Imports
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import UIDocument
+from pyrevit import revit, forms, script
+
+script_dir = os.path.dirname(__file__)
+ui_doc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document # Get the Active Document
+
+# MAIN SCRIPT
+
+# Option to Select Fire or Acosutic
