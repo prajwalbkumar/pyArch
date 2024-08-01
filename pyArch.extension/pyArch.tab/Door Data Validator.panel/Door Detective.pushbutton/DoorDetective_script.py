30d571143f128b6f43c9b368db36a3d89b044599
prajwalbkumar <ar.prajwalbkumar@gmail.com>
2024-08-01 10:49:42 +0530

    init door detective

--- pyArch.extension/pyArch.tab/Door Data Validator.panel/Door Detective.pushbutton/DoorDetective_script.py
+++ pyArch.extension/pyArch.tab/Door Data Validator.panel/Door Detective.pushbutton/DoorDetective_script.py
@@ -3,7 +3,6 @@
 __title__ = "Door Detective"
 __author__ = "prajwalbkumar"
 
-
 # Imports
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import UIDocument
@@ -12,6 +11,5 @@ from pyrevit import revit, forms, script
 ui_doc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document # Get the Active Document
 output = script.get_output()
-
 
 # MAIN SCRIPT
