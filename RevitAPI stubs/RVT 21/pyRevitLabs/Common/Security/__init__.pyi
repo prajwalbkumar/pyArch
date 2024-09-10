1c7da90c0c33d1f06e3fbdfa441c412130737582
prajwalbkumar <ar.prajwalbkumar@gmail.com>
2024-09-10 11:39:40 +0530

    Stubbs Exited

--- RevitAPI stubs/RVT 21/pyRevitLabs/Common/Security/__init__.pyi
+++
@@ -1,5 +0,0 @@
-from typing import Tuple, Set, Iterable, List
-
-
-class UserAuth:
-    def UserIsInSecurityGroup(targetSid: str) -> bool: ...
--- RevitAPI stubs/RVT 21/pyRevitLabs/PyRevit/Controls/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Tuple, Set, Iterable, List
-
-
-class PyRevitWindow:
-    def __init__(self): ...
-    def OnApplyTemplate(self) -> None: ...
