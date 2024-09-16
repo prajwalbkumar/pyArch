f098fa47bf2f63845618d3da4638578e7d6d74b0
prajwalbkumar <ar.prajwalbkumar@gmail.com>
2024-09-16 18:43:43 +0530

    All Metadata Updated

--- pyArch.extension/pyArch.tab/Modelling Aids.panel/Allign Elements.stack/Delete Rooms.pushbutton/DeleteRooms_script.py
+++ pyArch.extension/pyArch.tab/Modelling Aids.panel/Allign Elements.stack/Delete Rooms.pushbutton/DeleteRooms_script.py
@@ -30,6 +30,9 @@ for room in all_rooms:
         else:
             not_enclosed_rooms.append(room)
 
+input1 = ""
+input2 = ""
+
 if not_placed_rooms:
     input1 = forms.alert("There are NOT PLACED rooms in the model \n"
                         "Would you like to delete them?", title = "Delete Rooms", warn_icon = True, options = ["Ok", "No"])
@@ -37,31 +40,40 @@ if not_placed_rooms:
 if not_enclosed_rooms:
     input2 = forms.alert("There are NOT ENCLOSED rooms in the model \n"
                         "Would you like to delete them?", title = "Delete Rooms", warn_icon = True, options = ["Ok", "Show Report"])
-    if input2 == "Show Report":
-        failed_data = []
-        for room in not_enclosed_rooms:
-            failed_data.append([output.linkify(room.Id),room.Level.Name.upper(),room.LookupParameter("Name").AsString().upper(),room.LookupParameter("Number").AsString().upper()])
-            
-        output.print_md("##⚠️ Not Enclosed Rooms Found ☹️") # Markdown Heading 2
-        output.print_md("---") # Markdown Line Break
-        output.print_md("❌ There are Issues in your Model.")  # Print a Line
-        output.print_table(table_data=failed_data, columns=["ELEMENT ID","LEVEL", "ROOM NAME", "ROOM NUMBER"]) # Print a Table
-        output.print_md("---") # Markdown Line Break
 
-    if input1 == "Ok":
-        t = Transaction(doc, "Delete Not Placed Rooms")
-        t.Start()
-        for room in not_placed_rooms:
-            doc.Delete(room.Id)
-        t.Commit()
+if input2 == "Show Report":
+    failed_data = []
+    for room in not_enclosed_rooms:
+        failed_data.append([output.linkify(room.Id),room.Level.Name.upper(),room.LookupParameter("Name").AsString().upper(),room.LookupParameter("Number").AsString().upper()])
+        
+    output.print_md("##⚠️ Not Enclosed Rooms Found ☹️") # Markdown Heading 2
+    output.print_md("---") # Markdown Line Break
+    output.print_md("❌ There are Issues in your Model.")  # Print a Line
+    output.print_table(table_data=failed_data, columns=["ELEMENT ID","LEVEL", "ROOM NAME", "ROOM NUMBER"]) # Print a Table
+    output.print_md("---") # Markdown Line Break
 
-    if input2 == "Ok":
-        t = Transaction(doc, "Delete Not Enclosed Rooms")
-        t.Start()
-        for room in not_enclosed_rooms:
-            doc.Delete(room.Id)
-        t.Commit()
+count = 0
 
+if input1 == "Ok":
+    t = Transaction(doc, "Delete Not Placed Rooms")
+    t.Start()
+    for room in not_placed_rooms:
+        count += 1
+        doc.Delete(room.Id)
+    t.Commit()
+
+if input2 == "Ok":
+    t = Transaction(doc, "Delete Not Enclosed Rooms")
+    t.Start()
+    for room in not_enclosed_rooms:
+        count += 1
+        doc.Delete(room.Id)
+    t.Commit()
+
+if count:
+    forms.alert("{} Room(s) have been deleted" .format(count), title = "Rooms Deleted", warn_icon = False)
+if not count:
+    forms.alert("No Rooms Deleted", title = "Run Successful", warn_icon = False)
 
 
 
