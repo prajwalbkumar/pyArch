b0ba7f2b901c1d93e43cc63a1b676677d6a31b66
prajwalbkumar <ar.prajwalbkumar@gmail.com>
2024-10-25 19:19:20 +0530

    Fire and STC Rating Fix

--- pyArch.extension/pyArch.tab/Doors.panel/Schedule.stack/Fire_RatingCheck.pushbutton/FireRatingCheck_script.py
+++ pyArch.extension/pyArch.tab/Doors.panel/Schedule.stack/Fire_RatingCheck.pushbutton/FireRatingCheck_script.py
@@ -118,10 +118,12 @@ try:
 
     # Check if all Doors have Fire Rating. If Not:
     failed_doors = []
+    skipped_doors = []
+    skipped_data = []
 
     manual_time = manual_time + 150
     total_element_count = total_element_count + len(door_collector) 
-
+    count = 0
     for door in door_collector:
         try:
             door_rating_param = door.LookupParameter("Fire_Rating")
@@ -133,18 +135,20 @@ try:
                         if hosted_wall_rating.AsString() == "" or hosted_wall_rating.AsString() == "0":
                             continue
                         else:
+                            count = count + 1
                             if door_rating_param.AsString() == str(int(int(hosted_wall_rating.AsString())*0.75)):
                                 continue
                             else:
                                 failed_doors.append(door)
         except:
+            skipped_doors.append(door)
             continue
-
-    if failed_doors:
-        report = forms.alert("Door fire ratings require attentions", title="Door Fire Rating Missing", warn_icon=True, options=["Show Report","Auto-Fill Values"]) 
+    
+    if skipped_doors:
+        report = forms.alert("Door fire ratings require attentions", title="Door Fire Rating Missing", warn_icon=True, options=["Show Report"]) 
         failed_data = []
 
-        for door in failed_doors:
+        for door in skipped_doors:
                 hosted_wall_rating = door.Host.LookupParameter("Fire_Rating")
                 if door.LookupParameter("Mark"):
                     if not door.LookupParameter("Mark").HasValue or door.LookupParameter("Mark").AsString() == "": 
@@ -175,53 +179,107 @@ try:
                 else:
                     door_room_number = "NONE"
 
-                failed_data.append([output.linkify(door.Id), door_mark, door_level, door_room_name, door_room_number, hosted_wall_rating.AsString(), door.LookupParameter("Fire_Rating").AsString(), str(int(int(hosted_wall_rating.AsString())*0.75))])
+                failed_data.append([output.linkify(door.Id), door_mark, door_level, door_room_name, door_room_number, hosted_wall_rating.AsString(), door.LookupParameter("Fire_Rating").AsString(), "INVALID HOST RATING"])
 
         if report == "Show Report":
             output.print_md("##⚠️ {} Completed. Instances Need Attention ☹️" .format(__title__)) # Markdown Heading 2
             output.print_md("---") # Markdown Line Break
             output.print_md("❌ Some issues could not be resolved. Refer to the **Table Report** below for reference")  # Print a Line
-            output.print_table(table_data=failed_data, columns=["ELEMENT ID", "MARK", "LEVEL", "ROOM NAME", "ROOM NUMBER", "HOST WALL RATING", "CURRENT RATING", "CORRECT RATING"]) # Print a Table
+            output.print_table(table_data=failed_data, columns=["ELEMENT ID", "MARK", "LEVEL", "ROOM NAME", "ROOM NUMBER", "HOST WALL RATING", "CURRENT RATING", "ERROR"]) # Print a Table
             print("\n\n")
             output.print_md("---") # Markdown Line Break
 
-        if report == "Auto-Fill Values":
-            t = Transaction(doc, "Filling Door Fire Ratings")
-            t.Start()
-            for door in failed_doors:
-                wall = door.Host
-                wall_fire_param = wall.LookupParameter("Fire_Rating").AsString()
-                door.LookupParameter("Fire_Rating").Set(str(int(int(wall_fire_param)*0.75)))
-            t.Commit()
-            
-            success_message = "Fire Rating of " + str(len(failed_doors)) + " doors have been filled"
-            success = forms.alert(success_message, title="Parameters Updated", warn_icon=False, options=["Show Report", "Ok"])
+    else:
+        if failed_doors:
+            report = forms.alert("Door fire ratings require attentions", title="Door Fire Rating Missing", warn_icon=True, options=["Show Report","Auto-Fill Values"]) 
+            failed_data = []
 
-            if success == "Show Report":
-                output.print_md("##✅ {} Completed. 😊" .format(__title__)) # Markdown Heading 2
+            for door in failed_doors:
+                    hosted_wall_rating = door.Host.LookupParameter("Fire_Rating")
+                    if door.LookupParameter("Mark"):
+                        if not door.LookupParameter("Mark").HasValue or door.LookupParameter("Mark").AsString() == "": 
+                            door_mark = "NONE"
+                        else:
+                            door_mark = door.LookupParameter("Mark").AsString().upper()
+                    else:
+                        door_mark = "NONE"
+                    
+                    if door.LookupParameter("Level"):
+                        door_level = door.LookupParameter("Level").AsValueString().upper()
+                    else:
+                        door_level = "NONE"
+                    
+                    if door.LookupParameter("Room_Name"):
+                        if not door.LookupParameter("Room_Name").HasValue or door.LookupParameter("Room_Name").AsString() == "": 
+                            door_room_name = "NONE"
+                        else:
+                            door_room_name = door.LookupParameter("Room_Name").AsString().upper()
+                    else:
+                        door_room_name = "NONE"
+                    
+                    if door.LookupParameter("Room_Number"):
+                        if not door.LookupParameter("Room_Number").HasValue or door.LookupParameter("Room_Number").AsString() == "": 
+                            door_room_number = "NONE"
+                        else:
+                            door_room_number = door.LookupParameter("Room_Number").AsString().upper()
+                    else:
+                        door_room_number = "NONE"
+
+                    failed_data.append([output.linkify(door.Id), door_mark, door_level, door_room_name, door_room_number, hosted_wall_rating.AsString(), door.LookupParameter("Fire_Rating").AsString(), str(int(int(hosted_wall_rating.AsString())*0.75))])
+
+            if report == "Show Report":
+                output.print_md("##⚠️ {} Completed. Instances Need Attention ☹️" .format(__title__)) # Markdown Heading 2
                 output.print_md("---") # Markdown Line Break
-                output.print_md("✅ Door ratings have been updated. Refer to the **Table Report** below for reference")  # Print a Line
-                output.print_table(table_data=failed_data, columns=["ELEMENT ID","MARK", "LEVEL", "ROOM NAME", "ROOM NUMBER", "HOST WALL RATING", "PREVIOUS RATING", "UPDATED RATING"]) # Print a Table
+                output.print_md("❌ Some issues could not be resolved. Refer to the **Table Report** below for reference")  # Print a Line
+                output.print_table(table_data=failed_data, columns=["ELEMENT ID", "MARK", "LEVEL", "ROOM NAME", "ROOM NUMBER", "HOST WALL RATING", "CURRENT RATING", "CORRECT RATING"]) # Print a Table
                 print("\n\n")
                 output.print_md("---") # Markdown Line Break
 
-    elif not unowned_elements:
-        forms.alert("All door ratings correct", title="Door Rating Check", warn_icon=False) 
+            if report == "Auto-Fill Values":
 
-    if unowned_elements:
-        unowned_element_data = []
-        for element in unowned_elements:
-            try:
-                unowned_element_data.append([output.linkify(element.Id), element.Category.Name.upper(), "REQUEST OWNERSHIP", WorksharingUtils.GetWorksharingTooltipInfo(doc, element.Id).Owner])
-            except:
-                pass
+                t = Transaction(doc, "Filling Door Fire Ratings")
+                t.Start()
+                
+                for index, door in enumerate(failed_doors):
+                    wall = door.Host
+                    wall_fire_param = wall.LookupParameter("Fire_Rating").AsString() 
+                    door.LookupParameter("Fire_Rating").Set(str(int(int(wall_fire_param)*0.75)))
 
-        output.print_md("##⚠️ Elements Skipped ☹️") # Markdown Heading 2
-        output.print_md("---") # Markdown Line Break
-        output.print_md("❌ Make sure you have Ownership of the Elements - Request access. Refer to the **Table Report** below for reference")  # Print a Line
-        output.print_table(table_data = unowned_element_data, columns=["ELEMENT ID", "CATEGORY", "TO-DO", "CURRENT OWNER"]) # Print a Table
-        print("\n\n")
-        output.print_md("---") # Markdown Line Break
+                t.Commit()
+                
+                success_message = "Fire Rating of " + str(len(failed_doors)) + " doors have been filled"
+                success = forms.alert(success_message, title="Parameters Updated", warn_icon=False, options=["Show Report", "Ok"])
+
+                if success == "Show Report":
+                    output.print_md("##✅ {} Completed. 😊" .format(__title__)) # Markdown Heading 2
+                    output.print_md("---") # Markdown Line Break
+                    output.print_md("✅ Door ratings have been updated. Refer to the **Table Report** below for reference")  # Print a Line
+                    output.print_table(table_data=failed_data, columns=["ELEMENT ID","MARK", "LEVEL", "ROOM NAME", "ROOM NUMBER", "HOST WALL RATING", "PREVIOUS RATING", "UPDATED RATING"]) # Print a Table
+                    print("\n\n")
+                    output.print_md("---") # Markdown Line Break
+
+
+        elif not unowned_elements:
+            if count > 0:
+                forms.alert("All door ratings correct", title="Door Rating Check", warn_icon=False) 
+            else:
+                forms.alert("No wall ratings found in the project\n"
+                            "No door ratings updated.", title="Door Rating Check", warn_icon=False) 
+
+        if unowned_elements:
+            unowned_element_data = []
+            for element in unowned_elements:
+                try:
+                    unowned_element_data.append([output.linkify(element.Id), element.Category.Name.upper(), "REQUEST OWNERSHIP", WorksharingUtils.GetWorksharingTooltipInfo(doc, element.Id).Owner])
+                except:
+                    pass
+
+            output.print_md("##⚠️ Elements Skipped ☹️") # Markdown Heading 2
+            output.print_md("---") # Markdown Line Break
+            output.print_md("❌ Make sure you have Ownership of the Elements - Request access. Refer to the **Table Report** below for reference")  # Print a Line
+            output.print_table(table_data = unowned_element_data, columns=["ELEMENT ID", "CATEGORY", "TO-DO", "CURRENT OWNER"]) # Print a Table
+            print("\n\n")
+            output.print_md("---") # Markdown Line Break
 
     end_time = time.time()
     runtime = end_time - start_time
